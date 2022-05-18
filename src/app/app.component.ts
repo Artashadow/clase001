b6cf43907f865f65b1eed67eb2b6ca1fd856cccc
Pablo Rosales <pablo.rosales@scanda.com.mx>
2022-05-18 13:03:37 -0500

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,24 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ChildActivationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'clase001';
-  contador: number;
-  
-  constructor() {
-    console.log("Constructor inicializando contador ... ");
-    this.contador = 0;
-  }
-
-  ngOnInit() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngOnInit", this.contador);
-  }
-
-  ngOnChange() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngOnChange", this.contador);
-  }
-
-  ngAfterContentInit() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngAfterContentInit", this.contador);
-  }
-
-  ngAfterContentChecked() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngAfterContentChecked", this.contador);
-  }
-
-  ngAfterViewInit() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngAfterViewInit", this.contador);
-  }
-
-  ngAfterViewChecked() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngAfterViewChecked", this.contador);
-  }
-
-  ngOnDestroy() {
-    this.contador++;
-    console.log("Ciclo de vida, entrando al ... ngOnDestroy", this.contador);
-  }
-
+    title = 'sb-admin-angular';
+    constructor(public router: Router, private titleService: Title) {
+        this.router.events
+            .pipe(filter(event => event instanceof ChildActivationEnd))
+            .subscribe(event => {
+                let snapshot = (event as ChildActivationEnd).snapshot;
+                while (snapshot.firstChild !== null) {
+                    snapshot = snapshot.firstChild;
+                }
+                this.titleService.setTitle(snapshot.data.title || 'SB Admin Angular');
+            });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
-import { OtherComponentComponent } from './components/other-component/other-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OtherComponentComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ComponentsModule
-    ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
